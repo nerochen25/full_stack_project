8ff6c0033b73abe776250039ed97c1ea9e71d2a9
Nero <nerochen25@berkeley.edu>
2018-12-14 09:50:35 -0800

    Modify the session_api since user route changed

--- frontend/util/session_api_util.js
+++ frontend/util/session_api_util.js
@@ -23,7 +23,7 @@ export const login = user => (
 export const signup = user => (
   $.ajax({
     method: 'POST',
-    url: '/api/user',
+    url: '/api/users',
     data: {
       user: {
         email_address: user.emailAddress,
