6422769c784fffbfc74b92fadac9c20a41cb2f0d
Nero <nerochen25@berkeley.edu>
2019-01-27 17:32:53 -0800

    Add pledge model

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -1,3 +1,12 @@
 class Pledge < ApplicationRecord
-    
+    validates :amount, :project, :supporter, presence: true
+    validates_uniqueness_of :supporter_id, scope: :project_id, message: 'Please edit your existing pledge.'
+  
+    belongs_to :supporter,
+      class_name: 'User',
+      foreign_key: :supporter_id
+  
+    belongs_to :project,
+      class_name: 'Project',
+      foreign_key: :project_id
 end
