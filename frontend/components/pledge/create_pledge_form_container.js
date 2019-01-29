c52e3ef13e207234e4a2181d442d35fa64729ec1
Nero <nerochen25@berkeley.edu>
2019-01-28 16:32:00 -0800

    Add pledges into entities reducer

--- frontend/components/pledge/create_pledge_form_container.js
+++ frontend/components/pledge/create_pledge_form_container.js
@@ -1,7 +1,10 @@
 import { connect } from 'react-redux';
 import CreatePledgeForm from './create_pledge_form';
+import { createPledge } from '../../actions/pledge_actions';
+import { fetchPledges } from '../../actions/pledge_actions';
   
 const mapStateToProps = (state, ownProps) => {
+    debugger
     return {
         currentUserId: state.session.id,
     };
@@ -9,7 +12,8 @@ const mapStateToProps = (state, ownProps) => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        
+        action: pledge => dispatch(createPledge(pledge)),
+        fetchPledges: () => dispatch(fetchPledges())
     };
 };
 
--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,13 +1,13 @@
-import {
-  combineReducers
-}
-from 'redux';
+import { combineReducers } from 'redux';
 import users from './users_reducer';
 import projects from './projects_reducer';
 import categories from './categories_reducer';
+import pledges from './pledges_reducer';
+
 
 export default combineReducers({
   users,
   projects,
   categories,
+  pledges,
 });
