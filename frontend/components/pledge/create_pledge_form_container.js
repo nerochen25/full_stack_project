073c3b4b1da318c60d0c989b250de09c1df6f049
Nero <nerochen25@berkeley.edu>
2019-01-28 18:05:38 -0800

    Modify variable type for the pledge container

--- frontend/components/pledge/create_pledge_form_container.js
+++ frontend/components/pledge/create_pledge_form_container.js
@@ -4,9 +4,9 @@ import { createPledge } from '../../actions/pledge_actions';
 import { fetchPledges } from '../../actions/pledge_actions';
   
 const mapStateToProps = (state, ownProps) => {
-    const project = state.entities.projects[ownProps.match.params.id];
-    const projectId = project.id;
-    const creator = state.entities.users[project.creator_id];
+    var project = state.entities.projects[ownProps.match.params.id];
+    var projectId = project.id;
+    var creator = state.entities.users[project.creator_id];
     return {
         pledge: {
             amount: '',
